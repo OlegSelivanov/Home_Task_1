1c2088cc913a152bc0e20cb1c35dd0d4c2ac51f5
Oleg Selivanov <alien3569@mail.ru>
2022-11-29 01:42:13 +0300

    Add Task_6 Start

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -4,11 +4,5 @@
 
 Console.Clear();
 
-Console.WriteLine("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите третье число: ");
-int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
