ceafc86d84a1e2bb5616cf19accd3fe4528c1a19
Oleg Selivanov <alien3569@mail.ru>
2022-11-29 01:38:03 +0300

    Add Task_6 Start

---
+++ Task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+является ли число чётным (делится ли оно на два без остатка).
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
