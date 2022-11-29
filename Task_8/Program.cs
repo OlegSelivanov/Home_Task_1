3801699e0de323e130de07034fe0c666709c65ff
Oleg Selivanov <alien3569@mail.ru>
2022-11-29 18:19:22 +0300

    Fin

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -6,3 +6,13 @@ Console.Clear();
 
 Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i}");
+    }
+    i ++;
+}
