fab25fea1085b673de772e712656f6eacdff422d
Oleg Selivanov <alien3569@mail.ru>
2022-11-29 17:12:54 +0300

    Add Fin

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -12,3 +12,11 @@ int b = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a
+
+if (a > max) max = a:
+else if (b > max) max = b;
+else if (c > max) max = c;
+
+Console.Write($"Максимальное число = {max}")
